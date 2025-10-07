8773b3262b42c226782bd32e4a699b7aad745f37
Australia Server <australiaserver150@gmail.com>
2025-10-07 04:30:33 +0000

    Again try please

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,7 @@
-import { initializeApp } from "firebase/app";
+
+"use client";
+
+import { initializeApp, getApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -13,8 +16,11 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const provider = new GoogleAuthProvider();
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const provider = new GoogleAuthProvider();
+
+export { app, auth, db, storage, provider };
