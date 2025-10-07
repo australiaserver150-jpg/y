3cfe7ac234962dc547f9e789ba16329359b7a6fd
Australia Server <australiaserver150@gmail.com>
2025-10-07 11:59:38 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,8 @@
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+export const firebaseConfig = {
+  "projectId": "studio-7625668114-17a40",
+  "appId": "1:629206608253:web:153a54a197cfda478e8414",
+  "apiKey": "AIzaSyBoVCNKTe8qgtB8BwTNAr0u8Ec7eO3GCKM",
+  "authDomain": "studio-7625668114-17a40.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "629206608253"
 };
-
-export default firebaseConfig;
